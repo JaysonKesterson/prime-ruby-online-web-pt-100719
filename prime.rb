56e2e1ebdeaee66ffd3a54eb8f2221c27e1db0f8
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-16 13:51:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,30 @@
 # Add  code here!
+def prime?(number)
+  
+  if number == 2
+    return true
+  end
+  
+  if number < 2
+    return false
+  end
+  
+  count = 2
+  highnum = number / count
+  range = []
+  boolean = nil
+  
+  while count < highnum do
+   range << count
+   count += 1
+  end
+  
+  range.each do |num|
+    if number % num == 0
+      boolean = false
+    else
+      boolean = true
+    end
+  end
+  boolean
+end
